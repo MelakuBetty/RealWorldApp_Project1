f41113cc325fa93450e03638ba75c0432912897a
betty melaku <melakubetty@gmail.com>
2022-05-28 23:15:36 +0300

    delete sanity

--- Tests/test_web/test_signin.py
+++ Tests/test_web/test_signin.py
@@ -8,8 +8,8 @@ import allure
 @pytest.mark.usefixtures('init_driver')
 class TestSignin(Base):
 
-    @pytest.mark.sanity
-    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    # @pytest.mark.sanity
+    @allure.severity(allure.severity_level.CRITICAL)
     @allure.description("Validates 'Real World App' valid signin credentials")
     def test_signin_successfully(self):
 
@@ -17,8 +17,8 @@ class TestSignin(Base):
         sign_in(self, "yuvi", "123456")
         assertion(self, SigninLocators.massage, SigninLocators.txt)
 
-    @pytest.mark.sanity
-    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    # @pytest.mark.sanity
+    @allure.severity(allure.severity_level.CRITICAL)
     @allure.description("Validates 'Real World App' Null signin credentials")
     def test_signin_unsuccessfully(self):
 
@@ -27,8 +27,8 @@ class TestSignin(Base):
         assertion_buttons(self, SigninLocators.signin_button)
         assertion(self, SigninLocators.empty_uname, SigninLocators.txt_error)
 
-    @pytest.mark.sanity
-    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    # @pytest.mark.sanity
+    @allure.severity(allure.severity_level.CRITICAL)
     @allure.description("Validates 'Real World App' invalid signin credentials")
     def test_signin_unsuccessfully1(self):
 
@@ -37,8 +37,8 @@ class TestSignin(Base):
         assertion(self, SigninLocators.invalid_user_pass, SigninLocators.txt_invalid)
 
 
-    @pytest.mark.sanity
-    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    # @pytest.mark.sanity
+    @allure.severity(allure.severity_level.CRITICAL)
     @allure.description("Validates 'Real World App' invalid signin credentials")
     def test_signin_unsuccessfully2(self):
 
@@ -46,8 +46,8 @@ class TestSignin(Base):
         sign_in(self, "yuvi", "2556")
         assertion(self, SigninLocators.invalid_user_pass, SigninLocators.txt_invalid)
 
-    @pytest.mark.sanity
-    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    # @pytest.mark.sanity
+    @allure.severity(allure.severity_level.CRITICAL)
     @allure.description("Validates 'Real World App' invalid signin credentials")
     def test_signin_unsuccessfully3(self):
 
